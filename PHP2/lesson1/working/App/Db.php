58de29432fbf430e9f5f3894c3eca45c8c77bcf9
Pavel02 <pavel.hr@mail.ru>
2017-01-21 19:12:43 +0300

    + метод execute() класса Db

--- PHP2/lesson1/working/App/Db.php
+++ PHP2/lesson1/working/App/Db.php
@@ -5,8 +5,21 @@ namespace App;
 
 class Db
 {
+    protected $dbh;
+
     public function __construct()
     {
-        echo 'Hello, DB!';
+        // автоматически создается соединение к базе и возвращаем соединение в свойство объекта
+        $this->dbh = new \PDO('mysql:host=127.0.0.1;dbname=testphp2', 'root', '');
     }
+
+    public function execute($sql)    // Хочется иметь метод, выполняющий любой sql запрос к БД
+    {
+        $sth = $this->dbh->prepare($sql);   // Возвращается подготовленный запрос
+        $res = $sth->execute();             // Результат true или false (т.е. удалось ли выполнение запроса)
+        return $res;
+
+    }
+
+
 }
--- PHP2/lesson1/working/index.php
+++ PHP2/lesson1/working/index.php
@@ -3,4 +3,7 @@
 require __DIR__ . '/autoload.php';
 
 $db = new \App\Db();
+$res = $db->execute('CREATE TABLE foo (id SERIAL)');
+var_dump($res);                                             // Вначале выведет bool(true)
+                                                            // Если обновить страницу, то уже bool(false)
 
