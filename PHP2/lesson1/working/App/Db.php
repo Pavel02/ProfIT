7775c1279909dde107da4a56ebc2e4320bb6f3e6
Pavel02 <pavel.hr@mail.ru>
2017-01-22 22:14:27 +0300

    + метод query() класса Db

--- PHP2/lesson1/working/App/Db.php
+++ PHP2/lesson1/working/App/Db.php
@@ -13,13 +13,21 @@ class Db
         $this->dbh = new \PDO('mysql:host=127.0.0.1;dbname=testphp2', 'root', '');
     }
 
-    public function execute($sql)    // Хочется иметь метод, выполняющий любой sql запрос к БД
+    public function execute($sql)    // Хочется иметь метод, выполняющий любой sql запрос к БД/
     {
         $sth = $this->dbh->prepare($sql);   // Возвращается подготовленный запрос
         $res = $sth->execute();             // Результат true или false (т.е. удалось ли выполнение запроса)
         return $res;
-
+                                            // Данный метод будем использовать, когда не нужно возвращать данные
     }
 
-
+    public function query($sql)
+    {
+        $sth = $this->dbh->prepare($sql);   // Подготовили выражение
+        $res = $sth->execute();             // Выполнили запрос
+        if ($res !== false) {               // Если результат выражения не false
+            return $sth->fetchAll();        // Вызываем метод fetchAll, который получит все данные результата запроса и эти данные вернем
+        }
+        return [];                          // В противном случае вернем пустой массив
+    }
 }
--- PHP2/lesson1/working/index.php
+++ PHP2/lesson1/working/index.php
@@ -3,7 +3,13 @@
 require __DIR__ . '/autoload.php';
 
 $db = new \App\Db();
-$res = $db->execute('CREATE TABLE foo (id SERIAL)');
-var_dump($res);                                             // Вначале выведет bool(true)
+// $res = $db->execute('CREATE TABLE foo (id SERIAL)');      // Для промежуточной отладки
+// var_dump($res);                                             // Вначале выведет bool(true)
                                                             // Если обновить страницу, то уже bool(false)
+
+$data = $db->query('SELECT * FROM foo');
+
+var_dump($data);
+
+
 
