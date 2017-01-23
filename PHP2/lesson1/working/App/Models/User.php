7030dd3865c6c52e019e59aef01ef8a3fff8efd9
PL <pavel.hr@mail.ru>
2017-01-23 11:49:52 +0300

    + ::findAll()

--- PHP2/lesson1/working/App/Models/User.php
+++ PHP2/lesson1/working/App/Models/User.php
@@ -2,8 +2,19 @@
 
 namespace App\Models;
 
+use App\Db;
+
 class User
 {
     public $email;
     public $name;
+
+    public static function findAll()    // Добавляем статич метод
+    {
+        $db = new Db();
+        return $db->query(
+            'SELECT * FROM users',
+            'App\Models\User'
+        );
+    }
 }
